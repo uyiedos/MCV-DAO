61fa22c5023ec93fefdb9206fdc785f2a95fe6bf
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2023-02-19 17:15:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ const App = () => {
   // Use the hooks thirdweb give us.
   const address = useAddress();
   console.log("👋 Address:", address);
-  const editionDropAddress = "0xc0FDE8e26e1cF78E9e0581D3cDB5d3b94FFAF34c"
+  // Initialize our Edition Drop contract
+  const editionDropAddress = "INSERT_EDITION_DROP_ADDRESS";
   const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
   // Hook to check if the user has our NFT
   const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
@@ -14,28 +15,19 @@ const App = () => {
     return nftBalance && nftBalance.gt(0)
   }, [nftBalance])
 
-
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to MCVDAO</h1>
+        <h1>Welcome to NarutoDAO</h1>
         <div className="btn-hero">
           <ConnectWallet />
         </div>
       </div>
     );
   }
-// Add this little piece!
-if (hasClaimedNFT) {
-  return (
-    <div className="member-page">
-      <h1>🍪DAO Member Page</h1>
-      <p>Congratulations on being a member</p>
-    </div>
-  );
-};
+
   // Render mint nft screen.
   return (
     <div className="mint-nft">
