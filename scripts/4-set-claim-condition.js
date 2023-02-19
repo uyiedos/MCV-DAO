dca1d4f40de673b11eb4086526660bbd137e6959
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2023-02-19 14:30:47 +0100

    Update 4-set-claim-condition.js

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,28 @@
+import sdk from "./1-initialize-sdk.js";
+import { MaxUint256 } from "@ethersproject/constants";
+
+(async () => {
+  try {
+    const editionDrop = await sdk.getContract("0xc0FDE8e26e1cF78E9e0581D3cDB5d3b94FFAF34c", "edition-drop");
+    // We define our claim conditions, this is an array of objects because
+    // we can have multiple phases starting at different times if we want to
+    const claimConditions = [{
+      // When people are gonna be able to start claiming the NFTs (now)
+      startTime: 03/01/2023(),
+      // The maximum number of NFTs that can be claimed.
+      maxClaimable: 5000,
+      // The price of our NFT (free)
+      price: 0.003,
+      // The amount of NFTs people can claim in one transaction.
+      maxClaimablePerWallet: 1,
+      // We set the wait between transactions to unlimited, which means
+      // people are only allowed to claim once.
+      waitInSeconds: MaxUint256,
+    }]
+
+    await editionDrop.claimConditions.set("0", claimConditions);
+    console.log("✅ Sucessfully set claim condition!");
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})();
