102ff1eb138ccf8d137251f1fd7de8c1d14d052d
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2023-02-20 19:19:07 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 // Import thirdweb provider and Goerli ChainId
-import { ThirdwebProvider } from '@thirdweb-dev/react';
-import { ChainId } from '@thirdweb-dev/sdk';
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Goerli;
 
 // Wrap your app with the thirdweb provider
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ThirdwebProvider desiredChainId={activeChainId}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>,
+  document.getElementById("root")
 );
