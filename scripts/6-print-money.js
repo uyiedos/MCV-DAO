9e20c7013da47b8627039f2ab0f3d365331dd52e
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2023-02-20 21:12:12 +0100

    Update 6-print-money.js

--- scripts/6-print-money.js
+++ scripts/6-print-money.js
@@ -1,18 +1,20 @@
 import sdk from "./1-initialize-sdk.js";
 
-(async () => {
-  try {
-    // This is the address of our ERC-20 contract printed out in the step before.
-    const token = await sdk.getContract("0x6CE8913B8139B44ea43C2509b0eEf03835fF6345");
-    // What's the max supply you want to set? 1,000,000 is a nice number!
-    const amount = 2_000_000_000;
-    // Interact with your deployed ERC-20 contract and mint the tokens!
-    await token.mint(amount);
-    const totalSupply = await token.totalSupply();
+const token = sdk.getToken("0x6CE8913B8139B44ea43C2509b0eEf03835fF6345");
 
-    // Print out how many of our token's are out there now!
-    console.log("✅ There now is", totalSupply.displayValue, "$MCVD in circulation");
-  } catch (error) {
-    console.error("Failed to print money", error);
-  }
+(async () => {
+    try {
+        // max token supply
+        const amount = 2_000_000_000;
+        // interact with the deployed ERC-20 contract and mint the tokens
+        await token.mintToSelf(amount);
+        const totalSupply = await token.totalSupply();
+
+        // printing to the console how many tokens now exist
+        console.log("💸 There are now ", totalSupply.displayValue, "$MCVD in circulation");  
+    } catch (error){
+        console.error("😿 Failed to print money", error);  
+    }
 })();
+
+ 
