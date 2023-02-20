1d7e3a5f851dfec3e98fb670c53b8cfdd4967061
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2023-02-20 22:09:34 +0100

    Update 6-print-money.js

--- scripts/6-print-money.js
+++ scripts/6-print-money.js
@@ -4,10 +4,12 @@ import sdk from "./1-initialize-sdk.js";
   try {
     // This is the address of our ERC-20 contract printed out in the step before.
     const token = await sdk.getContract("0x6CE8913B8139B44ea43C2509b0eEf03835fF6345");
+    const address = "{{0x6CE8913B8139B44ea43C2509b0eEf03835fF6345}}"";
     // What's the max supply you want to set? 1,000,000 is a nice number!
     const amount = 2000000000;
     // Interact with your deployed ERC-20 contract and mint the tokens!
     await token.mint(amount);
+    const tx = await program.mintTo(address, 2000000000);
     const totalSupply = await token.totalSupply();
 
     // Print out how many of our token's are out there now!
