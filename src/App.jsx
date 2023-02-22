ccc196a75a1b206fa0dbeb289a3adc2b7478f77b
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2023-02-22 22:03:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useAddress, ConnectWallet, useContract, useNFTBalance } from '@thirdweb-dev/react';
+import { useAddress, ConnectWallet, useContract, useState, useEffect, useMemo, useNFTBalance } from '@thirdweb-dev/react';
 import { useState, useEffect, useMemo } from 'react';
 
 const App = () => {
