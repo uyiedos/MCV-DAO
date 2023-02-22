18c13af661085ebceb449af7cb1ee845e40ac6f2
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2023-02-22 20:55:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useAddress, ConnectWallet } from '@thirdweb-dev/react';
+import { useAddress, ConnectWallet, useContract, useNFTBalance } from '@thirdweb-dev/react';
+import { useState, useEffect, useMemo } from 'react';
 
 const App = () => {
   // Use the hooks thirdweb give us.
   const address = useAddress();
   console.log("👋 Address:", address);
+  // Initialize our Edition Drop contract
+  const editionDropAddress = "0xc0FDE8e26e1cF78E9e0581D3cDB5d3b94FFAF34c"
+  const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
+  // Hook to check if the user has our NFT
+  const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
+
+  const hasClaimedNFT = useMemo(() => {
+    return nftBalance && nftBalance.gt(0)
+  }, [nftBalance])
+
+  // ... include all your other code that was already there below.
 
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
@@ -17,13 +29,50 @@ const App = () => {
       </div>
     );
   }
-
-  // This is the case where we have the user's address
-  // which means they've connected their wallet to our site!
+  
+  if (!address) {
   return (
     <div className="landing">
-      <h1>👀 wallet connected, now what!</h1>
-    </div>);
+      <h1>Welcome to MCVDAO</h1>
+      <div className="btn-hero">
+        <ConnectWallet />
+      </div>
+    </div>
+  );
+}
+
+// Add this little piece!
+if (hasClaimedNFT) {
+  return (
+    <div className="member-page">
+      <h1>🍪DAO Member Page</h1>
+      <p>Congratulations on being a member</p>
+    </div>
+  );
+};
+
+  // Render mint nft screen.
+  return (
+    <div className="mint-nft">
+      <h1>Mint your free 🍪MCV Membership NFT</h1>
+      <div className="btn-hero">
+        <Web3Button 
+          contractAddress={editionDropAddress}
+          action={contract => {
+            contract.erc1155.claim(0, 1)
+          }}
+          onSuccess={() => {
+            console.log(`🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`);
+          }}
+          onError={error => {
+            console.error("Failed to mint NFT", error);
+          }}
+        >
+          Mint your NFT
+        </Web3Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
