66668c0a6c729fbf2534b6ef1e6345075cc60c45
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2023-02-22 23:25:09 +0100

    Update 8-deploy-vote.js

--- scripts/8-deploy-vote.js
+++ scripts/8-deploy-vote.js
@@ -1,24 +1,40 @@
-import sdk from './1-initialize-sdk.js';
+import sdk from "./1-initialize-sdk.js";
 
 (async () => {
-    try {
-        const voteContractAddress = await sdk.deployer.deployVote({
-            // Giving the governance contract a name
-            name: "MCV DAO Governance Contract",
+  try {
+    const voteContractAddress = await sdk.deployer.deployVote({
+      // Give your governance contract a name.
+      name: "MCV DAO governance",
 
-            // assigning the location property of the governance token (hint it's @ the ERC-20 contract address) 
-            voting_token_address: "0xeEe746dcE397378567039d845740D9bf28Fb399D",
-            voting_delay_in_blocks: 0,
-            // 1 day = 6570 blocks
-            voting_period_in_blocks: 6570,
-            voting_quorum_fraction: 0,
-        proposal_token_threshold: 0,
-        });
+      // This is the location of our governance token, our ERC-20 contract!
+      voting_token_address: "0x6CE8913B8139B44ea43C2509b0eEf03835fF6345",
 
-        console.log("Successfully deployed vote contract, address:",
-        voteContractAddress,
-        );
-    } catch (err){
-        console.error("Failed to deploy vote contract.", err);
-    }
+      // These parameters are specified in number of blocks.
+      // Assuming block time of around 13.14 seconds (for Ethereum)
+
+      // After a proposal is created, when can members start voting?
+      // For now, we set this to immediately.
+      voting_delay_in_blocks: 0,
+
+      // How long do members have to vote on a proposal when it's created?
+      // we will set it to 1 day = 6570 blocks
+      voting_period_in_blocks: 6570,
+
+      // The minimum % of the total supply that need to vote for
+      // the proposal to be valid after the time for the proposal has ended.
+      voting_quorum_fraction: 0,
+
+      // What's the minimum # of tokens a user needs to be allowed to create a proposal?
+      // I set it to 0. Meaning no tokens are required for a user to be allowed to
+      // create a proposal.
+      proposal_token_threshold: 0,
+    });
+
+    console.log(
+      "Successfully deployed vote contract, address:",
+      voteContractAddress
+    );
+  } catch (err) {
+    console.error("Failed to deploy vote contract", err);
+  }
 })();
